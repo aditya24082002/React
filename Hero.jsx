b88e0901d064ad29162a89b90ce6e289f545d7e5
Aditya KUmar <131743188+aditya24082002@users.noreply.github.com>
2024-05-25 02:06:36 +0530

    React project-1

---
+++ Hero.jsx
@@ -0,0 +1,32 @@
+const Herosection = () => {
+    return (
+        <main className="hero container">
+        <div className="hero-content">
+            <h1>Your feet deserve the best</h1>
+            <p>The same will happen to your body if you choose a shoe with a squishy base. Test a shoe's foundation by gently bending it at the toe. If it folds in half, that's a sign it's too soft. If it bends somewhat but stays mostly rigid, it has good support.
+ </p>
+     <div className="hero-btn">
+        <button>Shop Now</button>
+        <button className="secondary-btn">Category</button>
+     </div>
+     <div className="Shopping">
+        <p>Also available on</p>
+
+    <div className="brond-icons">
+    <img src="/image/download.jpg" alt="flipkart-logo" />
+    <img src="/image/amazon.jpg" alt="amazon-logo" />
+
+    </div>
+
+
+
+     </div>
+        </div>
+        <div className="hero-image">
+            <img src="/image/nike2.jpg" alt="hero-image" />
+        </div>
+        </main>
+    )
+};
+
+export default Herosection;
